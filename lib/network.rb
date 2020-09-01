8f5de4dd6a4fe3c0803e70ff3a39a9cac7c53c71
John Kim <abcdefghijohnk@gmail.com>
2020-09-01 10:39:29 -0600

    get main character test pass

--- lib/network.rb
+++ lib/network.rb
@@ -9,4 +9,28 @@ class Network
   def add_show(show)
     @shows << show
   end
+
+  def main_characters
+    main = []
+    all_characters.each do |char|
+      if char.salary >= 500000 && char.name == char.name.upcase
+        main << char
+      end
+    end
+    main
+  end
+
+  def all_characters
+    all_chars = []
+    @shows.each do |chars|
+      all_chars << chars.characters
+    end
+    all_chars.flatten!
+  end
+
+  #   def characters_by_show
+  #     @shows.map do |show|
+  #       show.characters
+  #     end
+  #   end
 end
